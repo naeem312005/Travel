fba1c5689ffe0bb1752ba5d75de573629d74c2b0
MD.Naimul Hasan Naeem <155387786+naeem312005@users.noreply.github.com>
2024-10-12 00:58:10 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+(function ($) {
+  "use strict";
+
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    margin: 24,
+    dots: true,
+    loop: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+    },
+  });
+})(jQuery);
